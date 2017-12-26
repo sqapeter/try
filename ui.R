b4f552fdd3e7bea4022ba56949029d7de2968fb6
sqapeter <cscpeter13@gmail.com>
2017-12-26 08:55:03 +0800

    commit from issue 2

--- ui.R
+++ ui.R
@@ -3,7 +3,8 @@ library(shiny)
 library(leaflet)
 
 shinyUI(fluidPage(
-  titlePanel("try try try"),
+  titlePanel("try issue 2"),
+  # titlePanel("try try try"),
   # titlePanel("Pipe Map"),
   leafletOutput('targetData',width=1000,height=800)
   
